b6ed68632ecd8b3700a4a5bc1e1f4bc2e0678aa6
Ashutosh Kumar <68853189+S-AshutoshDev@users.noreply.github.com>
2020-08-28 17:05:51 +0530

    GUI improvements

--- main.js
+++ main.js
@@ -1,15 +1,21 @@
 onload = () => {
-    document.querySelector(`.hamBtn`).addEventListener(`click`, () => {
-        document.querySelector(`.hamBtn`).classList.toggle(`activeHamBtn`)
-        document.querySelector(`.sideSection`).classList.toggle(`menuOpen`)
+    const menuButton = document.querySelector(`header .menuButton`)
+    const closeButton = document.querySelector(`header .closeButton`)
+    const menuBox = document.querySelector(`header .menu`)
+    menuButton.addEventListener(`click`, () => {
+        console.log(true)
+        menuBox.classList.toggle(`hidden`)
+        menuButton.classList.toggle(`hidden`)
+        closeButton.classList.toggle(`hidden`)
+
     })
+    closeButton.addEventListener(`click`, () => {
+        console.log(true)
+        menuBox.classList.toggle(`hidden`)
+        menuButton.classList.toggle(`hidden`)
+        closeButton.classList.toggle(`hidden`)
 
-
-
-
-
-
-
+    })
 
 
 
